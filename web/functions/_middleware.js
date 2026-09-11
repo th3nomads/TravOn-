cf908f73b632c7c1843aedb37cd67dc87acd0c01
th3nomads <th3nomadscreate@gmail.com>
2026-09-11 11:03:28 -0400

    Apply compact desktop styling sitewide

--- web/functions/_middleware.js
+++ web/functions/_middleware.js
@@ -4,6 +4,11 @@ export async function onRequest(context) {
   if (!contentType.includes("text/html")) return response;
 
   return new HTMLRewriter()
+    .on("head", {
+      element(element) {
+        element.append('<link rel="stylesheet" href="/desktop-compact.css?v=20260911-compact1">', { html: true });
+      }
+    })
     .on("body", {
       element(element) {
         element.append('<script src="/shared-remove.js?v=20260910-remove-shared1"></script><script src="/itinerary-label.js?v=20260910-singular1"></script><script src="/capitalize-itinerary.js?v=20260910-cap1"></script><script src="/admin-stats.js?v=20260910-admin1"></script>', { html: true });
