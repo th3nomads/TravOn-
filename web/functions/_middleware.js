be16ec36f493ccc37055d02dd65b80ab1849ca67
th3nomads <th3nomadscreate@gmail.com>
2026-09-11 11:33:43 -0400

    Load panoramic desktop hero

--- web/functions/_middleware.js
+++ web/functions/_middleware.js
@@ -6,12 +6,12 @@ export async function onRequest(context) {
   return new HTMLRewriter()
     .on("head", {
       element(element) {
-        element.append('<link rel="stylesheet" href="/desktop-compact.css?v=20260911-compact1">', { html: true });
+        element.append('<link rel="stylesheet" href="/desktop-compact.css?v=20260911-panorama1">', { html: true });
       }
     })
     .on("body", {
       element(element) {
-        element.append('<script src="/shared-remove.js?v=20260910-remove-shared1"></script><script src="/itinerary-label.js?v=20260910-singular1"></script><script src="/capitalize-itinerary.js?v=20260910-cap1"></script><script src="/admin-stats.js?v=20260910-admin1"></script>', { html: true });
+        element.append('<script src="/shared-remove.js?v=20260910-remove-shared1"></script><script src="/itinerary-label.js?v=20260910-singular1"></script><script src="/capitalize-itinerary.js?v=20260910-cap1"></script><script src="/admin-stats.js?v=20260910-admin1"></script><script src="/desktop-panorama.js?v=20260911-panorama1"></script>', { html: true });
       }
     })
     .transform(response);
